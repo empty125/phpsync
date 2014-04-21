5938c7ce8f2b1cffd4e544ccc649c9463d713228
empty_ <xilei125@163.com>
2014-04-21 18:13:28 +0800

    init warpper

--- libs/client_wrapper.php
+++ libs/client_wrapper.php
@@ -6,5 +6,38 @@
  * @author xilei
  */
 class Sc_Client_Warpper {
-    //put your code here
+    
+    public function stream_open($path,$mode,$options,&$opened_path){
+          
+    }
+    
+    public function stream_read($count){
+        
+    }
+    
+    public function stream_write($data){
+        
+    }
+    
+    public function stream_eof(){
+        
+    }
+    
+    public function stream_seek(){
+        
+    }
+    
+    public function stream_tell(){
+        
+    }
+    
+    public function stream_flush(){
+        
+    }
+    
+    public function stream_close(){
+        
+    }
+    
+    
 }
