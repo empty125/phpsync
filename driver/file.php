56226155ce1d43cc37a774bd965e5390b749b22d
empty_ <xilei125@163.com>
2014-04-28 17:46:43 +0800

    file driver

--- driver/file.php
+++ driver/file.php
@@ -6,8 +6,17 @@
  */
 class Sc_Driver_File {
     
+    private $_basePath = '';
+    
+    private $_error = '';
+    
     public function __construct(){
-        
+        $basePath = Sc::$rootDir.'/data/.tracker_record';
+        if(!is_dir($basePath) && mkdir($basePath,0755,true)){
+            Sc_Log::record("[driver file] dir creation failed {$basePath}",  Sc_Log::ERROR);
+            throw new Exception('dir creation failed', -100);
+        }
+        $this->_basePath = $basePath;
     }
     
     public function add($data){
@@ -31,15 +40,14 @@ class Sc_Driver_File {
     }
     
     public function error(){
-        
+        return $this->_error;
     }
     
     public function close(){
         
-    }
-    
+    }   
+
     public function __destruct() {
         ;
     }
-    
 }
---
+++ sync/scp.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * ssh2.scp
+ *
+ * @author xilei
+ */
+class Sc_Sync_Scp {
+    
+    public function config($config){
+        
+    }
+}
